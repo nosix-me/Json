b7e1081af8bc4b5976c5bdada3105e7b40fce3dc
nosix-me <nosix.me@gmail.com>
2014-05-21 21:54:41 +0800

    v0.1.0

--- Json_test.go
+++ Json_test.go
@@ -1,4 +1,4 @@
-// Copyright 2013 Json authors
+// Copyright 2014 Json authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License"): you may
 // not use this file except in compliance with the License. You may obtain
